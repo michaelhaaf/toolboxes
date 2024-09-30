ff661e39fe293a1c28e2d6842ec231930d506fbc
michaelhaaf <michael.haaf@gmail.com>
2024-09-30 01:34:22 -0400

    pare down nix stuff for now, rely on host group for testing

--- toolboxes/cli/files/etc/profile.d/00-cli-firstrun.sh
+++ toolboxes/cli/files/etc/profile.d/00-cli-firstrun.sh
@@ -15,14 +15,6 @@ if test "$(id -u)" -gt "0"; then
     printf "%s[ OK ]%s\n" "${blue}" "${normal}"
   fi
 
-  if test ! -f /run/openrc/softlevel; then
-    printf "Setting up nix...\t\t\t "
-    sudo mkdir -p /run/openrc
-    sudo touch /run/openrc/softlevel
-    sudo usermod -a -G nix-users "${USER}"
-    printf "%s[ OK ]%s\n" "${blue}" "${normal}"
-  fi
-
   if test ! -f /etc/cli.firstrun; then
     sudo touch /etc/cli.firstrun
     printf "\ncli first run complete.\n\n"
